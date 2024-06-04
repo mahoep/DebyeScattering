086ab60183abd26af07c26e36c9238ef949cc187
mahoep <mahoep@users.noreply.github.com>
2024-06-04 09:19:23 -0400

    Updating algorithim for multi-layered sphere
    Changed how main_coated.f90 works. Previously, I would approximate the Mie terms by taking the Debye series summation (over p) to very high terms (200). Since this summation has a term raised to the (p-1) and is recursive any small error or numerical stability issue in l=1 or l=2 would propagate up to l=L while also being raised to increasingly larger exponents. This would cause the solution to blow up. Instead the code uses an alternative representation to obtain the Mie scattering approximation which does not require a sum over p. This approach seems to be much more stable. Also only the requested debye ray is saved. A LOT NEEDS TO BE REWRITTEN/UPDATED to accommodate this change. I am pushing for my own sanity as it appears to be working in its final desired state.

--- fortran/main_coated.f90
+++ fortran/main_coated.f90
@@ -5,33 +5,30 @@ program main_coated
     implicit none
     
     real(16), parameter :: pi_c=4.0_wp*atan(1.0_wp)
-    real(16), parameter   :: floorval=1.0e-50_wp
-    real(16), allocatable :: theta(:), S1a(:)
+    real(16), parameter   :: floorval=1.0e-10_wp
+    real(16), allocatable :: theta(:), S1a(:), S1pa(:), XV(:)
     real(16), allocatable :: pi(:,:), tau(:,:)
-    complex(16) :: y, m1, m2, alpha, beta
+    complex(16) :: y, m1, m2, alpha, beta, raysum
     complex(16), allocatable :: D1x(:), D1y(:), D3x(:), D3y(:), A13x(:), lnA13y(:)
-    integer :: n, Nmx, i, nang, P, Pc, pee, l
-    real(16) :: wavelength, k, x, en, XV(6), check
-    complex(16), allocatable :: S1(:), S2(:), ray(:,:), Q(:,:)
+    integer :: n, Nmx, i, nang, P, Pc, pee, l, layers
+    real(16) :: wavelength, k, x, en, check(2)
+    complex(16), allocatable :: S1(:), S2(:), S1p(:), S2p(:), ray(:,:), Q(:,:), M(:), Qp(:,:)
     complex(16) :: u0, u11, u13, u31, u33, A13y, umR121, umR212, a1, b1, R121(2), R212(2), T1(2)
-    complex(16) :: a1p, b1p, M(7)
+    complex(16) :: a1p, b1p
     real(16) :: start, finish
 
-    nang = 1801
-    M(1) = (1.35, 0)
-    M(2) = (1.3, 0)
-    M(3) = (1.25, 0)
-    M(4) = (1.2, 0)
-    M(5) = (1.15, 0)
-    M(6) = (1.1, 0)
-    M(7) = (1.0, 0)
 
-    XV(1) = 100
-    XV(2) = 200
-    XV(3) = 300
-    XV(4) = 400
-    XV(5) = 500
-    XV(6) = 600
+    layers = 1
+    nang = 1801
+
+    allocate(M(layers+1))
+    M = 1.33_wp
+    M(size(M)) = 1.0_wp
+
+    allocate(XV(layers))
+    do i = 1,layers
+        XV(i) = 100.0_wp*i
+    end do
 
     x = XV(size(XV))
     Nmx = int( x + 4.05_wp*(x**(1.0_wp/3.0_wp)) + 2.0_wp )
@@ -39,8 +36,8 @@ program main_coated
     k = 2*pi_c / wavelength
 
     P = 200
-    Pc = 0
-    allocate(theta(nang), S1(nang), S1a(nang))
+    Pc = 1
+    allocate(theta(nang), S1(nang), S1a(nang), S1p(nang), S1pa(nang))
     do i = 1, nang
       theta(i) = real((i - 1),kind=wp) * pi_c / real(nang-1,kind=wp)
     end do
@@ -50,12 +47,13 @@ program main_coated
     allocate(D1x(0:Nmx), D1y(0:Nmx), D3x(0:Nmx), D3y(0:Nmx), A13x(0:Nmx), lnA13y(0:Nmx))
     
 
-    allocate(ray(2,P), Q(size(XV), 2))
+    allocate(ray(2,P), Q(size(XV), 2), Qp(size(XV), 2))
 
     do n  = 1, Nmx
         a1p = 1.0_wp
         b1p = 1.0_wp
         en = real(n, kind=wp)
+        Q = 0.0_wp
         do l = 1,size(XV)
             m1 = M(l)
             m2 = M(l+1)
@@ -100,30 +98,48 @@ program main_coated
                 end if
 
                 if (l.eq.1) then
-                    do pee = 1, P
-                        ray(i, pee) = T1(i) * (R121(i))**real(pee-1,kind=wp)
-                        check = abs(R121(i)**real(pee-1,kind=wp))
+                    ! do pee = 1, P
+                    !     check(1) = abs(real(T1(i) * (R121(i))**real(pee-1,kind=wp)))
+                    !     check(2) = abs(aimag(T1(i) * (R121(i))**real(pee-1,kind=wp)))
 
-                        if  (check.le.floorval) then
-                            ray(i,pee+1:P) = 0.0_wp
-                            exit
-                        endif
-                    end do
-                    Q(l,i) = R212(i) + sum(ray(i,:))
+                    !     if  (check(1).le.floorval .or. isnan(check(1))) then
+                    !         ray(i, pee)%re = 0.0_wp
+                    !     else if (check(2).le.floorval .or. isnan(check(2))) then
+                    !         ray(i, pee)%im = 0.0_wp
+                    !     else
+                            ray(i, Pc) = T1(i) * (R121(i))**real(Pc-1,kind=wp)
+                    !     end if        
+                    ! end do
+                    ! raysum = sum(ray(i,:))
+                    Q(l,i) = R212(i) +  T1(i) / (1.0_wp - R121(i))
+                    Qp(l,i) = ray(i, Pc) 
                 else
-                    do pee = 1, P
-                        ray(i, pee) = (R121(i) * Q(l-1,i))**real(pee-1,kind=wp)
-                        check = abs((R121(i) * Q(l-1,i))**real(pee-1,kind=wp))
+                    ! do pee = 1, P
+                    !     check(1) = abs(real((R121(i) * Q(l-1,i))**real(pee-1,kind=wp)))
+                    !     check(2) = abs(aimag((R121(i) * Q(l-1,i))**real(pee-1,kind=wp)))
 
-                        if  (check.le.floorval) then
-                            ray(i,pee+1:P) = 0.0_wp
-                            exit
-                        endif
-                    end do
-                    Q(l,i) = R212(i) + T1(i)*Q(l-1,i) * sum(ray(i,:))
+                    !     if  (check(1).le.floorval .or. isnan(check(1))) then
+                    !         ray(i, pee)%re = 0.0_wp
+                    !     else if (check(2).le.floorval .or. isnan(check(2))) then
+                    !         ray(i, pee)%im = 0.0_wp
+                    !     else
+                            ray(i, Pc) = (R121(i) * Qp(l-1,i))**real(Pc-1,kind=wp)
+                    !     end if      
+                    ! end do
+                    ! raysum = sum(ray(i,:))
+                    Q(l,i) = R212(i) +  (T1(i) * Q(l-1,i)) / (1.0_wp - R121(i) * Q(l-1,i))
+                    Qp(l,i) = ray(i, Pc) * T1(i) * Q(l-1,i)
                 end if
             end do  
         end do  
+
+        if (Pc.eq.0) then
+            a1p = 0.5_wp - 0.5_wp*R212(1)
+            b1p = 0.5_wp - 0.5_wp*R212(2)
+        else
+            a1p =  -0.5_wp * Qp(size(XV),1)
+            b1p =  -0.5_wp * Qp(size(XV),2)
+        end if
 
         a1 = 0.5_wp * ( 1.0_wp - Q(size(XV),1) )
         b1 = 0.5_wp * ( 1.0_wp - Q(size(XV),2) )
@@ -131,22 +147,26 @@ program main_coated
         S1 = S1 + (2.0_wp*en+1.0_wp)/(en*(en+1.0_wp)) * (a1*pi(n,:) + b1*tau(n,:))
         S2 = S1 + (2.0_wp*en+1.0_wp)/(en*(en+1.0_wp)) * (a1*tau(n,:) + b1*pi(n,:))
 
+        S1p = S1p + (2.0_wp*en+1.0_wp)/(en*(en+1.0_wp)) * (a1p*pi(n,:) + b1p*tau(n,:))
+        ! S2p = S2p + (2.0_wp*en+1.0_wp)/(en*(en+1.0_wp)) * (a1p*tau(n,:) + b1p*pi(n,:))
+
     end do
 
     S1a = abs(S1)**2.0_wp
+    S1pa = abs(S1p)**2.0_wp
 
     write(*,*) "writing... output.dat"
     open(10,file="output.dat",status="unknown")
     write(10,*)
-    write(10,*) "ang (deg)"," S1"
+    write(10,*) "ang (deg)"," S1", "S1(P=Pc)"
 
     do i=1,nang
-      write(10,*) theta(i),S1a(i)
+      write(10,*) theta(i),S1a(i),S1pa(i)
     enddo
     write(10,*)
     close(10)
     
-    deallocate(theta, pi, tau, D1x, D1y, D3x, D3y, A13x, lnA13y, S1, S2, ray)
+    ! deallocate(theta, pi, tau, D1x, D1y, D3x, D3y, A13x, lnA13y, S1, ray)
 
     stop
 
